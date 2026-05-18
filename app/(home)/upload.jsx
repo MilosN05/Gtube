6cbd033f22b9f85c0fb1fc2cb0806c1dc355ea84
MilosN05 <ninkovicmilos148@gmail.com>
2026-05-19 01:54:31 +0200

    novikomittt

--- app/(home)/upload.jsx
+++ app/(home)/upload.jsx
@@ -1,12 +1,23 @@
 import { Image } from 'expo-image';
 import { LinearGradient } from 'expo-linear-gradient';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Button, Modal, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { AnimatedCircularProgress } from 'react-native-circular-progress';
 
 export default function uploadF() {
 
   const [visible, setVisible] = useState(false);
   const [link, setLink] = useState("");
+  const [dProgress, uProgress] = useState(0)
+
+  const progressRef = useRef(null)
+
+  useEffect(() => {
+    progressRef.current?.animate(dProgress, 300)
+  }, [dProgress])
+  //Objasnjenje, useRef čuva vrednost (nonstop) od adrese prvog objekta i to u okviru progressRef.current, stoga
+  //Samo sazivamo .animate() nad jednim te istim objektom dodeljivajući mu dProgress vrednost
+  //Drugi parametar od .animate() samo označava kojom brzinom će da izanimira pomeranje do odgovarajućeg procenta !
   return (
     <LinearGradient
           colors={["#68493C", "#32202E", "#111425"]}
@@ -21,29 +32,49 @@ export default function uploadF() {
 
     <Modal visible={visible} animationType="slide" transparent={true} >
                         <View style={{flex:1 ,padding: 20, display:"flex",  backgroundColor:"transparent", gap:20, justifyContent:"center", alignItems:"center"}}>
-                            <View style={{backgroundColor:"#171b2f", padding:50, borderRadius:30, display:"flex",  elevation:50, gap:20, width:"100%", height:"40%", justifyContent:"center", alignItems:"center"}}>
+                            <View style={{backgroundColor:"#171b2f", padding:50, borderRadius:30, display:"flex",  elevation:50, gap:80, width:"100%", height:"60%", justifyContent:"center", alignItems:"center"}}>
                         {/* <View style={{}}>
                             <Text style={{fontFamily:"Montserrat", letterSpacing:2}} >
                             {"Kreatora ovog dela (Milos N.) tj. mene\nje izuzetno mrzelo da ovde dorađujem povratak šifre. Biće jednog dana dodato."}
                             </Text>
                         </View> */}
-    
+                        <AnimatedCircularProgress
+                        ref={progressRef} //Ovde ref dodeljuje adresu objekta vezan za AnimatedCircularProgress !
+                        size={120}
+                        width={15}
+                        // key={dProgress}
+                        fill={0}
+                        tintColor="#00e0ff"
+                        // onAnimationComplete={() => console.log('onAnimationComplete')}
+                        backgroundColor="#3d5875" />
+                          
                         <View style={{backgroundColor:"#3F4158", borderRadius:25, flexDirection:"row", width:"100%" }}>
-                          <TextInput style={{color:"white",left:20, fontFamily:"MontserratItalic", fontSize:15, zIndex:3}} 
+                          <TextInput style={{color:"white",paddingLeft:20, paddingRight:20, fontFamily:"MontserratItalic", fontSize:15, zIndex:3}} 
                           placeholder="https://youtu.be/..." 
+                          onChangeText={(text)=>
+                            setLink(text)
+                          }
                           value={link}
                           placeholderTextColor={"#787996"}
                           onSubmitEditing={(text)=>{
-                            link = text.nativeEvent.text;
-                            if (link.startsWith("https://youtu.be")) {
+                            let linkR = text.nativeEvent.text;
+                            if (linkR.startsWith("https://youtu.be")) {
 
-                              socket = new  WebSocket("ws://192.168.101.128:8000/objavaSnimaka/")
+                              let socket = new  WebSocket("ws://192.168.174.128:8000/objavaSnimaka/")
                               socket.addEventListener("open", (event)=> {
-                                socket.send(JSON.stringify({link: link}))
+                                socket.send(JSON.stringify({link: linkR}))
                               })
 
                               socket.addEventListener("message", (event)=> {
-                                console.log(event.data)
+                               
+                                let message = JSON.parse(event.data).message
+                                console.log(event)
+                                if (message!="zavrseno") {
+                                  const percentage = parseFloat(message)
+                                  uProgress(percentage)
+                                }
+                                else console.log(message)
+                                
                               })
                             
                             }
