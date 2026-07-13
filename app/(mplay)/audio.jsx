a3f7d2aed1bc0945faf0c97b92b2c82f6c496274
MilosN05 <ninkovicmilos148@gmail.com>
2026-07-13 07:25:57 +0200

    komit_posle_dugo_vremena

--- app/(mplay)/audio.jsx
+++ app/(mplay)/audio.jsx
@@ -1,15 +1,109 @@
 import { Ionicons } from '@expo/vector-icons'
 import Slider from '@react-native-community/slider'
+import { Image } from 'expo-image'
 import { LinearGradient } from 'expo-linear-gradient'
 import { router } from 'expo-router'
-import { useState } from 'react'
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import { useAudioPro } from 'react-native-audio-pro'
+import { useEffect, useRef, useState } from 'react'
+import { Animated, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+
+import { Pressable } from 'react-native'
+import { AudioPro, AudioProContentType, AudioProState, useAudioPro } from 'react-native-audio-pro'
+import { play_next, play_previous, setTracks } from '../../setupAudio'
+
+
+async function get_data(funkcija) {
+  try {
+    const response = await fetch("http://192.168.1.68:8000/nasumicniZapisi/",
+      {
+      method:"POST",
+      body: new URLSearchParams(
+        {brojSnimaka:10, idSnimka:1}
+      ).toString(),
+      headers:{"Content-Type":"application/x-www-form-urlencoded"}})
+  
+    // console.log(response)
+  if (!response.ok) {
+    throw new Error(`HTTP greska ! Status: ${response.status}`)
+  }
+
+  const data = await response.json()
+  // data.artwork = "http://192.168.1.68:8000"+data.artwork
+  // data.url= "http://192.168.1.68:8000"+data.url
+  // data.url = "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"
+
+
+  data.forEach((data)=> {
+    data.artwork = "http://192.168.1.68:8000"+data.artwork
+    data.url= "http://192.168.1.68:8000"+data.url
+  })
+    // console.log(data)
+
+  funkcija(data)
+
+  setTracks(data)
+
+  
+  // AudioPro.pause()
+  }
+
+  catch (error) {
+    console.log("Fetch neuspesan !: ",error)
+  }
+}
+
+function to_seconds(ms) {
+  let sekunde=Math.trunc(Math.trunc(ms%60000) / 1000)
+  let minute = Math.trunc(ms/60000)
+  return sekunde>=10 ? `${minute}:${sekunde}`:`${minute}:0${sekunde}`
+}
 
 export function player() {
-
+    
     const { state, position, duration, playingTrack, playbackSpeed, volume, error } = useAudioPro();
     let [vrednost, setVrednost] = useState(0)
+    let [podaci, ucitaj_podatke] = useState({})
+    // console.log(playingTrack)
+    useEffect(()=> {
+
+      
+      AudioPro.configure({
+      contentType: AudioProContentType.MUSIC,
+      debug: __DEV__,
+    });
+      get_data(ucitaj_podatke)
+
+
+    },[])
+
+    useEffect(()=> {
+      if (podaci[0] && playingTrack && podaci[0].id!=playingTrack.id)
+        AudioPro.play(podaci[0])
+
+      else if (!playingTrack && podaci[0]) 
+        AudioPro.play(podaci[0])
+    },[podaci])
+
+
+
+
+    const opacity = useRef(new Animated.Value(1)).current;
+    
+    const toggleIcon = () => {
+      Animated.sequence([
+        Animated.timing(opacity, {
+          toValue: 0,
+          duration: 100,
+          useNativeDriver: true,
+        }),
+        Animated.timing(opacity, {
+          toValue: 1,
+          duration: 100,
+          useNativeDriver: true,
+        }),
+      ]).start();
+    };
+
+    // AudioPro.play(podaci)
     return (
       
        <LinearGradient
@@ -23,12 +117,12 @@ export function player() {
       <View style={{height:"40%", display:"flex", gap:40}}>
         <View style={{display:"flex", justifyContent:"space-between", alignItems:"center",  flexDirection:"row", marginTop:10}}>
          
-          <TouchableOpacity><Ionicons name="chevron-back-outline" size={32} color={"white"} onPress={()=> {router.back()}}></Ionicons></TouchableOpacity>
-          <TouchableOpacity><Ionicons name="options-outline"  size={32} color={"white"}></Ionicons></TouchableOpacity>
+          <TouchableOpacity onPress={()=> {router.back()}}><Ionicons name="chevron-back-outline" size={32} color={"white"} ></Ionicons></TouchableOpacity>
+          <TouchableOpacity onPress={()=>{}}><Ionicons name="options-outline"  size={32} color={"white"}></Ionicons></TouchableOpacity>
         </View>
         <View style={{display:"flex", alignItems:"center"}}>
           {/* <View style={{backgroundColor:"#442C33", display:"flex", alignItems:"center", borderRadius:10, paddingTop:7,paddingBottom:7}}> */}
-            <LinearGradient
+            {/* <LinearGradient
             colors={["#FFB86B", "#571c2b"]}
             start={{ x: 0, y: 0 }}
             end={{ x: 1, y: 1 }}
@@ -44,21 +138,28 @@ export function player() {
                 end={{ x: 1, y: 1 }}
         
                 style={{width:"230", height:"230", borderRadius:10}}/>
-          </LinearGradient>
+          </LinearGradient> */}
+
+          {/* url pesme ce ici */}
+          <Image 
+          source={{uri: `${playingTrack?.artwork}`}}
+          style={{width:230,height:230, borderRadius:10}}
+          
+          ></Image>
         </View>
       </View>
       <View style={{height:"60%"}}>
         <View style={{display:"flex", justifyContent:"center", alignItems:"center", paddingTop:20}}>
-          <Text style={{fontFamily:"MontserratBold", color:"white", fontSize:20}}>Lorem ipsum Dolor</Text>
+          <Text style={{fontFamily:"MontserratBold", color:"white", fontSize:20, textAlign:"center"}}>{playingTrack?.title}</Text>
           <View style={{display:"flex", justifyContent:"center", alignItems:"center"}}>
-            <Text style={{fontFamily:"Montserrat", color:"gray"}}>The Artist Name</Text>
-            <Text style={{fontFamily:"Montserrat", color:"gray"}}>Album Title</Text>
+            <Text style={{fontFamily:"Montserrat", color:"gray"}}>{playingTrack?.artist}</Text>
+            {/* <Text style={{fontFamily:"Montserrat", color:"gray"}}>Album Title</Text> */}
           </View>
         </View>
         <View style={{ paddingTop:50}}>
           <View style={{flexDirection:"row", display:"flex", justifyContent:"space-between"}}>
-            <TouchableOpacity><Ionicons name="heart-outline"size={30} color={"white"}></Ionicons></TouchableOpacity>
-            <TouchableOpacity><Ionicons name="volume-high-outline" size={30} color={"white"}></Ionicons></TouchableOpacity>
+            <TouchableOpacity onPress={()=>{}}><Ionicons name="heart-outline"size={30} color={"white"}></Ionicons></TouchableOpacity>
+            <TouchableOpacity onPress={()=>{}}><Ionicons name="volume-high-outline" size={30} color={"white"}></Ionicons></TouchableOpacity>
           </View>
           <View style={{display:"flex", justifyContent:"center", alignItems:"center"}}>
            <LinearGradient
@@ -71,9 +172,10 @@ export function player() {
 
         <Slider
           style={styles.slider}
-          onValueChange={setVrednost}
+          onValueChange={AudioPro.seekTo}
           minimumValue={0}
-          maximumValue={1}
+          value={position}
+          maximumValue={duration}
           thumbImage={require("../../assets/images/dotSlider32.png")}
           minimumTrackTintColor="transparent"
           maximumTrackTintColor="transparent"
@@ -84,30 +186,38 @@ export function player() {
           </Slider>
       </View>
       <View style={{display:"flex", flexDirection:"row", justifyContent:"space-between"}}>
-        <Text style={{fontFamily:"Montserrat", color:"white", fontSize:15}}>0:00</Text>
-        <Text style={{fontFamily:"Montserrat", color:"white", fontSize:15}}>3:25</Text>
+        <Text style={{fontFamily:"Montserrat", color:"white", fontSize:15}}>{to_seconds(position)}</Text>
+        <Text style={{fontFamily:"Montserrat", color:"white", fontSize:15}}>{to_seconds(duration)}</Text>
 
       </View>
-
+        
       <View style={{display:"flex", flexDirection:"row", justifyContent:"space-between", paddingTop:35, alignItems:"center"}}>
-        <TouchableOpacity><Ionicons name="reload-outline" color={"white"} size={30}></Ionicons></TouchableOpacity>
-        <TouchableOpacity><Ionicons name="play-back-outline" color={"white"} size={30}></Ionicons></TouchableOpacity>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={()=>{AudioPro.seekTo(0)}} ><Ionicons name="reload-outline" color={"white"} size={30} ></Ionicons></TouchableOpacity>
+        <Pressable onPress={()=>{play_previous()}}><Ionicons name="play-back-outline" color={"white"} size={30} ></Ionicons></Pressable>
         <LinearGradient 
         colors={["#FA9C6A", "#E2636F"]}
             
         style={{borderRadius:999, padding:20}}>
-          <Ionicons name="pause" color={"white"} size={30}></Ionicons>
+        <Animated.View style={{ opacity }}>
+          <Ionicons name={AudioPro.getState()==AudioProState.PLAYING ? `pause`:"play"} color={"white"} size={30} onPress={()=> {
+            
+            toggleIcon()
+            if (AudioPro.getState()==AudioProState.PLAYING)
+              AudioPro.pause()
+            else {
+              AudioPro.resume()
+            }
+          }}></Ionicons>
+        </Animated.View>
         </LinearGradient>
-        </TouchableOpacity>
-        <TouchableOpacity><Ionicons name="play-forward-outline" color={"white"} size={30}></Ionicons></TouchableOpacity>
-        <TouchableOpacity><Ionicons name="shuffle-outline" color={"white"} size={30}></Ionicons></TouchableOpacity>
+        <TouchableOpacity onPress={()=>play_next()}><Ionicons name="play-forward-outline" color={"white"} size={30} ></Ionicons></TouchableOpacity>
+        <TouchableOpacity onPress={()=>{}}><Ionicons name="shuffle-outline" color={"white"} size={30}></Ionicons></TouchableOpacity>
       </View>
           
       </View>
       
       <View style={{display:"flex",justifyContent:"center", alignItems:"center", paddingTop:50}}>
-        <TouchableOpacity style={{display:"flex",justifyContent:"center", alignItems:"center",}}>
+        <TouchableOpacity style={{display:"flex",justifyContent:"center", alignItems:"center",}} onPress={()=>{}}>
         <Ionicons name="chevron-up" color={"white"} size={32}></Ionicons>
         <Text style={{fontFamily:"Montserrat", color:"white"}}>Lyrics</Text>
         </TouchableOpacity>
--- setupAudio.js
+++ setupAudio.js
@@ -1,5 +1,27 @@
 import { AudioPro, AudioProContentType, AudioProEventType } from 'react-native-audio-pro';
 
+
+let tracks = []
+let current_index = 0
+let shuffle_index =0
+
+export function setTracks(track_of_songs) {
+	current_index =0
+	tracks = track_of_songs
+
+}
+
+
+export function play_previous() {
+	const nextTrackFromRemote_PREV = determineNextTrack(false);
+	AudioPro.play(nextTrackFromRemote_PREV);
+}
+
+export function play_next() {
+	const nextTrackFromRemote = determineNextTrack(true);
+	AudioPro.play(nextTrackFromRemote);
+}
+
 export function setupAudio() {
 	// Configure audio settings
 	AudioPro.configure({
@@ -15,7 +37,7 @@ export function setupAudio() {
 		switch (event.type) {
 			case AudioProEventType.TRACK_ENDED:
 				// Auto-play next track when current track ends
-				const nextTrack = determineNextTrack();
+				const nextTrack = determineNextTrack(true);
 				if (nextTrack) {
 					AudioPro.play(nextTrack);
 				}
@@ -23,13 +45,25 @@ export function setupAudio() {
 
 			case AudioProEventType.REMOTE_NEXT:
 				// Handle next button press from lock screen/notification
-				const nextTrackFromRemote = determineNextTrack();
-				AudioPro.play(nextTrackFromRemote);
+				play_next()
+				break;
+
+			case AudioProEventType.REMOTE_PREV:
+				play_previous()
 				break;
 		}
 	});
 }
 
-function determineNextTrack() {
+function determineNextTrack(napred) {
 	/* Your logic here */
+	// console.log(3)
+	// console.log(tracks)
+	if (napred && current_index<tracks.length-1)
+		current_index+=1
+	else if (!napred && current_index>0)
+		current_index-=1
+	
+	// console.log(current_index)
+	return tracks[current_index]
 }
